8de1c5a0a34489a18fd646e714908f6c7e8f2a6a
abikart <karthikabimanyu@hotmail.com>
2024-12-26 21:07:51 -0500

    feat(deploy): github pages

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	base: process.env.GITHUB_PAGES // Set base URL for GitHub Pages
+		? "/starter/" // Replace with your actual repository name
+		: "/",
+});
