e92aac00adedf6a5fdc3212eca410a8947a8d0ae
abikart <karthikabimanyu@hotmail.com>
2024-12-26 22:12:40 -0500

    feat(tests): setup

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -7,4 +7,14 @@ export default defineConfig({
 	base: process.env.GITHUB_PAGES // Set base URL for GitHub Pages
 		? "/starter/" // Replace with your actual repository name
 		: "/",
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: ["./src/test/setup.ts"],
+		coverage: {
+			provider: "v8",
+			reporter: ["text", "json", "html"],
+			exclude: ["node_modules/", "src/test/setup.ts"],
+		},
+	},
 });
--- packages/common/vite.config.ts
+++ packages/common/vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
-import { resolve } from "node:path";
 import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	build: { lib: { entry: resolve(__dirname, "src/main.ts"), formats: ["es"] } },
-	resolve: { alias: { src: resolve("src/") } },
 	plugins: [dts()],
+	build: {
+		lib: {
+			entry: "src/main.ts",
+			formats: ["es"],
+			fileName: "common",
+		},
+	},
+	test: {
+		coverage: {
+			provider: "v8",
+			reporter: ["text", "json", "html"],
+			exclude: ["node_modules/"],
+		},
+	},
 });
