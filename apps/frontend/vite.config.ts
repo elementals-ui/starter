259cfe2c2922675aaa13a990a587098994b30886
abikart <karthikabimanyu@hotmail.com>
2025-01-26 08:23:24 -0800

    fix(vite): config ts error ignore

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	// @ts-ignore
 	plugins: [react()],
 	base: process.env.GITHUB_PAGES // Set base URL for GitHub Pages
 		? "/starter/" // Replace with your actual repository name
